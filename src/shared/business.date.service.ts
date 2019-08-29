42e67cd97c90628c597bf516046628bcd7a58bd2
pharendarz <przemyslaw.harendarz@deutsche-boerse.com>
2019-08-29 17:02:27 +0200

    a lot of stuff

---
+++ src/shared/business.date.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core';
+
+import {asyncMap} from '@dbg-riskit/angular-common';
+import {Apollo} from 'apollo-angular';
+import {ApolloQueryResult} from 'apollo-client';
+import {Observable} from 'rxjs';
+import {distinctUntilChanged, shareReplay} from 'rxjs/operators';
+import {BusinessDate, BusinessDateGQL, Date} from './graphql.types';
+
+@Injectable()
+export class BusinessDateService {
+    private readonly _businessDate: Observable<Date>;
+
+    public constructor(private apollo: Apollo,
+                       private businessDateQuery: BusinessDateGQL) {
+        this._businessDate = this.apollo.watchQuery<BusinessDate.Query>({
+            query       : this.businessDateQuery.document,
+            pollInterval: 1000 * 60
+        }).valueChanges.pipe(
+            asyncMap((result: ApolloQueryResult<BusinessDate.Query>) => {
+                    return result.data.currentBusinessDate;
+                }
+            ),
+            distinctUntilChanged(),
+            shareReplay(1)
+        );
+    }
+
+    public get businessDate(): Observable<Date> {
+        return this._businessDate;
+    }
+}
